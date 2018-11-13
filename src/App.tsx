fadb8c48715ec73d5b192a0e4bbd4a251ba212f2
danursin <danursin@gmail.com>
2018-11-12 22:23:21 -0600

    three js cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,84 @@
 import * as React from "react";
+import * as THREE from "three";
 import "./App.css";
-import RubiksCube from "./features/RubiksCube";
-import Scene from "./features/Scene";
-import { Color, FaceDirection, Vector } from "./types";
-import { RubiksCubeObject } from "./types/RubiksCubeObject";
 
-interface IAppState {
-    cubeUnitLength: number;
-    rotate: Vector;
-    translate: Vector;
-    colors?: { [key in FaceDirection]?: Color };
-    perspective: number;
-}
-class App extends React.Component<{}, IAppState> {
+class App extends React.Component<{}> {
+    private mount: HTMLDivElement | null;
+    private scene: THREE.Scene;
+    private camera: THREE.Camera;
+    private cube: THREE.Mesh;
+    private frameID: number;
+    private renderer: THREE.WebGLRenderer;
     constructor(props: {}) {
         super(props);
 
-        const n = 50;
-        this.state = {
-            colors: { back: "blue", bottom: "yellow", front: "green", left: "orange", right: "red", top: "white" },
-            cubeUnitLength: n,
-            perspective: 100000,
-            rotate: [0, 0, 0],
-            translate: [-3 * n, -3 * n, 0]
-        };
-        this.handleRotateChange = this.handleRotateChange.bind(this);
-        this.handleTranslateChange = this.handleTranslateChange.bind(this);
-        this.handlePerspectiveChange = this.handlePerspectiveChange.bind(this);
+        this.animate = this.animate.bind(this);
     }
 
-    public handleRotateChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const name = event.target.name;
-        const split = name.split("-");
-        const value = +event.target.value;
-        const dimension = split[1] as keyof Vector;
-        this.setState({
-            rotate: {
-                ...this.state.rotate,
-                [dimension]: value
-            }
-        });
+    public componentDidMount() {
+        if (!this.mount) {
+            throw new Error("Component did not mount the mount");
+        }
+        const w = this.mount.clientWidth;
+        const h = this.mount.clientHeight;
+
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+        this.camera.position.z = 4;
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setClearColor("#000000");
+        this.renderer.setSize(w, h);
+        this.mount.appendChild(this.renderer.domElement);
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+
+        this.start();
     }
 
-    public handleTranslateChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const name = event.target.name;
-        const split = name.split("-");
-        const value = +event.target.value;
-        const dimension = split[1] as keyof Vector;
-        this.setState({
-            translate: {
-                ...this.state.translate,
-                [dimension]: value
-            }
-        });
-    }
-
-    public handlePerspectiveChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const value = +event.target.value;
-        this.setState({
-            perspective: value
-        });
+    public componentWillUnmount() {
+        this.stop();
+        if (!this.mount) {
+            throw new Error("Component did not mount the mount");
+        }
+        this.mount.removeChild(this.renderer.domElement);
     }
 
     public render() {
         return (
-            <div>
-                <Scene perspective={this.state.perspective}>
-                    <RubiksCube cube={new RubiksCubeObject(this.state.cubeUnitLength)} />
-                    {/* <Cube rotate={this.state.rotate} translate={this.state.translate} colors={this.state.colors} /> */}
-                </Scene>
-            </div>
+            <div
+                style={{ width: "400px", height: "400px" }}
+                ref={mount => {
+                    this.mount = mount;
+                }}
+            />
         );
+    }
+
+    private start() {
+        if (!this.frameID) {
+            this.frameID = requestAnimationFrame(this.animate);
+        }
+    }
+
+    private stop() {
+        cancelAnimationFrame(this.frameID);
+    }
+
+    private animate() {
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.renderScene();
+        this.frameID = window.requestAnimationFrame(this.animate);
+    }
+
+    private renderScene() {
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
